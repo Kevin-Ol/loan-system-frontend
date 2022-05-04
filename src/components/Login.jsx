34c31a8b8554239ccb1b269b9f26fc7862c66e33
Kevin Oliveira <kevin.zero@hotmail.com>
2022-05-03 21:37:43 -0300

    Página de Login

---
+++ src/components/Login.jsx
@@ -0,0 +1,42 @@
+import { useState, useCallback } from "react";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [revealPassword, setRevealPassword] = useState(false);
+
+  const handleEmail = useCallback(({ target }) => setEmail(target.value));
+  const handlePassword = useCallback(({ target }) => setPassword(target.value));
+  const handleRevealPassord = useCallback(({ target }) =>
+    setRevealPassword(target.checked)
+  );
+
+  return (
+    <form>
+      <label htmlFor="email">Email</label>
+      <input
+        type="email"
+        id="email"
+        placeholder="seu.email@gmail.com"
+        value={email}
+        onChange={handleEmail}
+      />
+      <label htmlFor="password">Senha</label>
+      <input
+        type={revealPassword ? "text" : "password"}
+        placeholder="senha123"
+        id="password"
+        value={password}
+        onChange={handlePassword}
+      />
+      <label htmlFor="reveal-password">Mostrar senha</label>
+      <input
+        type="checkbox"
+        id="reveal-password"
+        onChange={handleRevealPassord}
+      />
+    </form>
+  );
+}
+
+export default Login;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Login from "../components/Login";
+
+function App() {
+  return (
+    <main>
+      <h1>Login</h1>
+      <Login />
+    </main>
+  );
+}
+
+export default App;
