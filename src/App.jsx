c62c3c6ccd2bf68c027c4514bbafe38f1bc76b86
Kevin Oliveira <kevin.zero@hotmail.com>
2022-05-06 23:53:48 -0300

    Adiciona rota publica

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,21 @@ import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Loan from "./pages/Loan";
 import PrivateRoute from "./components/PrivateRoute";
+import PublicRoute from "./components/PublicRoute";
 import { AuthProvider } from "./context/Auth";
 
 function App() {
   return (
     <AuthProvider>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route element={<PrivateRoute />}>
-          <Route path="client/register" element={<Register />} />
-          <Route path="loan/list" element={<Loan />} />
+        <Route element={<PublicRoute />}>
+          <Route path="/" element={<Home />} />
         </Route>
-        <Route path="*" element={<Navigate to="/client/register" />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="loan/register" element={<Loan />} />
+          <Route path="client/register" element={<Register />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/loan/register" />} />
       </Routes>
     </AuthProvider>
   );
